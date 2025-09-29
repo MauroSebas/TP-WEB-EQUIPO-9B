aa9c0bfe425adf38ec60bc94d8196ca71b77a4dc
SebastianDte <sebateunmate@outlook.com>
2025-09-29 16:18:39 -0300

    Unión del trabajo de Mauro con el Proyecto base

--- SeleccionArticulo.aspx.designer.cs
+++ SeleccionArticulo.aspx.designer.cs
@@ -15,12 +15,12 @@ namespace TPWebForms_equipo9B
     {
 
         /// <summary>
-        /// Control rptArticulos.
+        /// Control btnSeleccionar.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater rptArticulos;
+        protected global::System.Web.UI.WebControls.Button btnSeleccionar;
     }
 }
