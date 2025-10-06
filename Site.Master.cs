c2985f6e01554cfd2e6def77bcaac817a6541235
SebastianDte <sebateunmate@outlook.com>
2025-10-06 12:14:35 -0300

    Validación Mail Footer

--- Site.Master.cs
+++ Site.Master.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,6 +12,31 @@ namespace TPWebForms_equipo9B
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+           
+        }
+        protected void btnSubscribe_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                lblSubscribeMsg.Text = "Campo vacío";
+            }
+            else
+            {
+                
+                string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+                if (Regex.IsMatch(txtEmail.Text, pattern))
+                {
+                    lblSubscribeMsg.Text = "Gracias por suscribirte!";                   
+                }
+                else
+                {
+                    lblSubscribeMsg.Text = "Email inválido!";
+                }
+            }
+
+
+
+
 
         }
     }
